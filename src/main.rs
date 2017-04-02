afe968dcb1c2a7c9b369d5c218dd47d37b78330a
Brian Gluzman <bgluzman@gmail.com>
2017-04-02 18:11:56 -0400

    color-mapped sphere

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,32 @@ extern crate assert_approx_eq;
 mod vec3;
 mod ray;
 
+use vec3::unit_vector;
+
 type Vec3 = vec3::Vec3<f64>;
 type Ray = ray::Ray<f64>;
 
-fn color(r: Ray) -> Vec3 {
-    let unit_direction = r.direction().unit_vector();
-    let t = 0.5 * (unit_direction.y() + 1.0);
-    Vec3::new(1.0, 1.0, 1.0) * (1.0 - t) + Vec3::new(0.5, 0.7, 1.0) * t
+fn hit_sphere(center: &Vec3, radius: f64, r: &Ray) -> f64 {
+    let oc = r.origin() - center;
+    let a = r.direction().dot(&r.direction());
+    let b = 2.0 * oc.dot(&r.direction());
+    let c = oc.dot(&oc) - radius * radius;
+    let d = b*b - 4.0*a*c;
+    if d < 0.0 { -1.0 } else { (-b - d.sqrt()) / (2.0 * a) }
+}
+
+fn color(r: &Ray) -> Vec3 {
+    let t = hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, &r);
+    if t > 0.0 {
+        let ref n0 = r.point_at_parameter(t) - Vec3::new(0.0, 0.0, -1.0);
+        let n = unit_vector(n0);
+        Vec3::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0) * 0.5
+    }
+    else {
+        let unit_direction = r.direction().unit_vector();
+        let t = 0.5*(unit_direction.y() + 1.0);
+        (1.0 - t)*Vec3::new(1.0, 1.0, 1.0) + t*Vec3::new(0.5, 0.7, 1.0)
+    }
 }
 
 fn main() {
@@ -28,7 +47,7 @@ fn main() {
             let v = j as f64 / ny as f64;
             let p = &lower_left_corner + &horizontal*u + &vertical*v;
             let r = Ray::new(&origin, &p);
-            let col = color(r);
+            let col = color(&r);
             let col_scaled = 255.99 * col;
             let (ir, ig, ib) = (col_scaled.r(), col_scaled.g(), col_scaled.b());
             println!("{} {} {}", ir as i32, ig as i32, ib as i32);
--- src/vec3.rs
+++ src/vec3.rs
@@ -239,6 +239,33 @@ impl<T: ElemT> Sub for Vec3<T> {
     }
 }
 
+impl<'a, T: ElemT> Sub<Vec3<T>> for &'a Vec3<T> {
+    type Output = Vec3<T>;
+    fn sub(self, rhs: Vec3<T>) -> Vec3<T> {
+        Vec3::new(self.x() - rhs.x(),
+                  self.y() - rhs.y(),
+                  self.z() - rhs.z())
+    }
+}
+
+impl<'a, 'b, T: ElemT> Sub<&'b Vec3<T>> for &'a Vec3<T> {
+    type Output = Vec3<T>;
+    fn sub(self, rhs: &'b Vec3<T>) -> Vec3<T> {
+        Vec3::new(self.x() - rhs.x(),
+                  self.y() - rhs.y(),
+                  self.z() - rhs.z())
+    }
+}
+
+impl<'a, T: ElemT> Sub<&'a Vec3<T>> for Vec3<T> {
+    type Output = Vec3<T>;
+    fn sub(self, rhs: &'a Vec3<T>) -> Vec3<T> {
+        Vec3::new(self.x() - rhs.x(),
+                  self.y() - rhs.y(),
+                  self.z() - rhs.z())
+    }
+}
+
 impl<T: ElemT> Mul for Vec3<T> {
     type Output = Vec3<T>;
     fn mul(self, rhs: Vec3<T>) -> Vec3<T> {
