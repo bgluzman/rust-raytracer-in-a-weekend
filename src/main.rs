5ffb65b97993c8d6e9c12a2ec15a5857c9131c97
Brian Gluzman <bgluzman@gmail.com>
2017-04-02 23:14:36 -0400

    Remove some comments

--- src/main.rs
+++ src/main.rs
@@ -20,27 +20,6 @@ type Hitable = hitable::Hitable<f64>;
 type HitRecord = hitable::HitRecord<f64>;
 type HitableList = hitablelist::HitableList<f64>;
 
-// fn hit_sphere(center: &Vec3, radius: f64, r: &Ray) -> f64 {
-//     let oc = r.origin() - center;
-//     let a = r.direction().dot(&r.direction());
-//     let b = 2.0 * oc.dot(&r.direction());
-//     let c = oc.dot(&oc) - radius * radius;
-//     let d = b*b - 4.0*a*c;
-//     if d < 0.0 { -1.0 } else { (-b - d.sqrt()) / (2.0 * a) }
-// }
-// 
-// fn color(r: &Ray) -> Vec3 {
-//     let t = hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, &r);
-//     if t > 0.0 {
-//         let ref n0 = r.point_at_parameter(t) - Vec3::new(0.0, 0.0, -1.0);
-//         let n = unit_vector(n0);
-//         return Vec3::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0) * 0.5;
-//     }
-//     let unit_direction = r.direction().unit_vector();
-//     let t = 0.5*(unit_direction.y() + 1.0);
-//     (1.0 - t)*Vec3::new(1.0, 1.0, 1.0) + t*Vec3::new(0.5, 0.7, 1.0)
-// }
-
 fn color(r: &Ray, world: &Hitable) -> Vec3 {
     let mut rec = HitRecord::new();
     if (*world).hit(&r, 0.0, f64::MAX, &mut rec) {
