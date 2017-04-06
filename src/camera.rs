e48954b5c9d0305229cca66ebe3b98f3d6601597
Brian Gluzman <bgluzman@gmail.com>
2017-04-05 21:06:36 -0400

    Ch10

--- src/camera.rs
+++ src/camera.rs
@@ -11,16 +11,19 @@ pub struct Camera<T: ElemT> {
 }
 
 impl<T: ElemT> Camera<T> {
-    pub fn new(vfov: T, aspect: T) -> Camera<T> {
+    pub fn new(lookfrom: Vec3<T>, lookat: Vec3<T>, vup: Vec3<T>, vfov: T, aspect: T) -> Camera<T> {
         let two = T::from_f64(2.).unwrap();
         let theta = vfov * T::from_f64(consts::PI/180.).unwrap();
         let half_height = (theta / two).tan();
         let half_width = aspect * half_height;
+        let w = (&lookfrom - &lookat).unit_vector();
+        let u = vup.cross(&w).unit_vector();
+        let v = w.cross(&u);
         Camera::<T> {
-            lower_left_corner: Vec3::new(-half_width, -half_height, -T::one()),
-            horizontal: Vec3::new(two * half_width, T::zero(), T::zero()),
-            vertical: Vec3::new(T::zero(), two * half_height, T::zero()),
-            origin: Vec3::default()
+            lower_left_corner: &lookfrom - &u*half_width - &v*half_height - &w,
+            horizontal: &u*half_width*two,
+            vertical: &v*half_height*two,
+            origin: lookfrom
         }
     }
 
--- src/main.rs
+++ src/main.rs
@@ -56,21 +56,24 @@ fn main() {
 
     println!("P3\n {} {} \n255", nx, ny);
 
-    // let list: Vec<Box<Hitable>> = vec![
-    //     Box::new(Sphere::new(Vec3::new(0., 0., -1.), 0.5, Box::new(Lambertian::new(Vec3::new(0.1, 0.2, 0.5))))),
-    //     Box::new(Sphere::new(Vec3::new(0., -100.5, -1.), 100., Box::new(Lambertian::new(Vec3::new(0.8, 0.8, 0.0))))),
-    //     Box::new(Sphere::new(Vec3::new(1., 0., -1.), 0.5, Box::new(Metal::new(Vec3::new(0.8, 0.6, 0.2), 0.)))),
-    //     Box::new(Sphere::new(Vec3::new(-1., 0., -1.), 0.5, Box::new(Dielectric::new(1.5)))),
-    //     Box::new(Sphere::new(Vec3::new(-1., 0., -1.), -0.45, Box::new(Dielectric::new(1.5))))
-    // ];
-    let R = (f64::consts::PI/4.).cos();
     let list: Vec<Box<Hitable>> = vec![
-        Box::new(Sphere::new(Vec3::new(-R,0.,-1.), R, Box::new(Lambertian::new(Vec3::new(0., 0., 1.))))),
-        Box::new(Sphere::new(Vec3::new( R,0.,-1.), R, Box::new(Lambertian::new(Vec3::new(1., 0., 0.)))))
+        Box::new(Sphere::new(Vec3::new(0., 0., -1.), 0.5, Box::new(Lambertian::new(Vec3::new(0.1, 0.2, 0.5))))),
+        Box::new(Sphere::new(Vec3::new(0., -100.5, -1.), 100., Box::new(Lambertian::new(Vec3::new(0.8, 0.8, 0.0))))),
+        Box::new(Sphere::new(Vec3::new(1., 0., -1.), 0.5, Box::new(Metal::new(Vec3::new(0.8, 0.6, 0.2), 0.)))),
+        Box::new(Sphere::new(Vec3::new(-1., 0., -1.), 0.5, Box::new(Dielectric::new(1.5)))),
+        Box::new(Sphere::new(Vec3::new(-1., 0., -1.), -0.45, Box::new(Dielectric::new(1.5))))
     ];
+    // let R = (f64::consts::PI/4.).cos();
+    // let list: Vec<Box<Hitable>> = vec![
+    //     Box::new(Sphere::new(Vec3::new(-R,0.,-1.), R, Box::new(Lambertian::new(Vec3::new(0., 0., 1.))))),
+    //     Box::new(Sphere::new(Vec3::new( R,0.,-1.), R, Box::new(Lambertian::new(Vec3::new(1., 0., 0.)))))
+    // ];
     let world = HitableList::new(list);
 
-    let cam = Camera::new(90., (nx as f64) / (ny as f64));
+    let cam = Camera::new(Vec3::new(-2., 2., 1.),
+                          Vec3::new(0., 0., -1.),
+                          Vec3::new(0., 1., 0.),
+                          15., (nx as f64) / (ny as f64));
 
     let mut rng = rand::thread_rng();
     for j in (0..ny).rev() {
